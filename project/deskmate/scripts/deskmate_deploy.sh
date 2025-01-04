d288de013152dd906517d2d5aff7f7f9db757788
lryain <47129927@qq.com>
2025-01-04 17:20:17 +0800

    add native deploy

---
+++ project/deskmate/scripts/deskmate_deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+set -u
+
+export GIT_PROJ_ROOT=`git rev-parse --show-toplevel`
+
+if [ $# -eq 1 -a $1 = -h ]; then
+    ${GIT_PROJ_ROOT}/project/deskmate/scripts/deploy.sh "$@"
+    exit 0
+fi
+
+${GIT_PROJ_ROOT}/project/deskmate/scripts/stage.sh "$@"
+
+# Also stage mateos
+if [ $? -eq 0 ]; then
+    PLATFORM_NAME=mateos ${GIT_PROJ_ROOT}/project/deskmate/scripts/stage.sh -k "$@"
+fi
+
+${GIT_PROJ_ROOT}/project/deskmate/scripts/deploy.sh "$@"
